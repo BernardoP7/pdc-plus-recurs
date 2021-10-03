61f50d7ef551f64d9865b3d67158f97ef7254036
Cordeir07 <bppedro39@gmail.com>
2021-10-03 16:41:16 +0100

    actualização do template

--- resources/views/header.blade.php
+++ resources/views/header.blade.php
@@ -21,45 +21,27 @@
           <!-- Collect the nav links, forms, and other content for toggling -->
           <div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
             <ul class="nav navbar-nav navbar-right main-menu">
-              <li class="dropdown"><a href="index.html">Home</a></li>
+              <li class="dropdown"><a href="/feed">Página inicial</a></li>
               <li class="dropdown">
-                <a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-haspopup="true" aria-expanded="false">Newsfeed <span><img src="assets/images/down-arrow.png" alt="" /></span></a>
+                <a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-haspopup="true" aria-expanded="false">Feed de notícias <span><img src="assets/images/down-arrow.png" alt="" /></span></a>
                   <ul class="dropdown-menu newsfeed-home">
-                    <li><a href="newsfeed.html">Newsfeed</a></li>
-                    <li><a href="newsfeed-people-nearby.html">Poeple Nearly</a></li>
-                    <li><a href="newsfeed-friends.html">My friends</a></li>
-                    <li><a href="newsfeed-messages.html">Chatroom</a></li>
-                    <li><a href="newsfeed-images.html">Images</a></li>
-                    <li><a href="newsfeed-videos.html">Videos</a></li>
+                    <li><a href="/feed">Feed de notícias</a></li>
+                    <li><a href="/conhecidos">Pessoas que talvez conheças</a></li>
+                    <li><a href="/feed-amigos">Amigos</a></li>
+                    <li><a href="/mensagens">Mensagens</a></li>
                   </ul>
               </li>
               <li class="dropdown">
-                <a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-haspopup="true" aria-expanded="false">Timeline <span><img src="assets/images/down-arrow.png" alt="" /></span></a>
+                <a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-haspopup="true" aria-expanded="false">Perfil <span><img src="assets/images/down-arrow.png" alt="" /></span></a>
                 <ul class="dropdown-menu login">
-                  <li><a href="timeline.html">Timeline</a></li>
-                  <li><a href="timeline-about.html">Timeline About</a></li>
-                  <li><a href="timeline-album.html">Timeline Album</a></li>
-                  <li><a href="timeline-friends.html">Timeline Friends</a></li>
+                  <li><a href="/perfil">Perfil</a></li>
+                  <li><a href="/sobre">Sobre</a></li>
+                  <li><a href="/albuns">Albuns</a></li>
+                  <li><a href="perfil-amigos">Amigos</a></li>
                 </ul>
               </li>
-              <li class="dropdown">
-                <a href="#" class="dropdown-toggle pages" data-toggle="dropdown" role="button" aria-haspopup="true" aria-expanded="false">All Pages <span><img src="assets/images/down-arrow.png" alt="" /></span></a>
-                <ul class="dropdown-menu page-list">
-                  <li><a href="index.html">Landing Page</a></li>
-                  <li><a href="newsfeed.html">Newsfeed</a></li>
-                  <li><a href="newsfeed-people-nearby.html">Poeple Nearly</a></li>
-                  <li><a href="newsfeed-friends.html">My friends</a></li>
-                  <li><a href="newsfeed-messages.html">Chatroom</a></li>
-                  <li><a href="newsfeed-images.html">Images</a></li>
-                  <li><a href="newsfeed-videos.html">Videos</a></li>
-                  <li><a href="timeline.html">Timeline</a></li>
-                  <li><a href="timeline-about.html">Timeline About</a></li>
-                  <li><a href="timeline-album.html">Timeline Album</a></li>
-                  <li><a href="timeline-friends.html">Timeline Friends</a></li>
-                  <li><a href="contact.html">Contact Us</a></li>
-                </ul>
-              </li>
-              <li class="dropdown"><a href="contact.html">Contact</a></li>
+              
+              <li class="dropdown"><a href="contact.html">Contactos</a></li>
             </ul>
             <form class="navbar-form navbar-right hidden-sm">
               <div class="form-group">
--- routes/web.php
+++ routes/web.php
@@ -15,12 +15,41 @@ use Illuminate\Support\Facades\Route;
 
 
 
-Auth::routes();
+//Auth::routes();
 
 Route::get('/', function () {
-    return view('home');
+    return view('feed/feed');
+});
+Route::get('/feed', function () {
+    return view('feed/feed');
+});
+Route::get('/conhecidos', function () {
+    return view('feed/conhecidos');
+});
+Route::get('/feed-amigos', function () {
+    return view('feed/amigos');
+});
+Route::get('/mensagens', function () {
+    return view('feed/mensagens');
 });
 
-Route::get('/child',function(){
-    return view('child');
+Route::get('/perfil', function () {
+    return view('perfil/perfil');
 });
+
+Route::get('/sobre', function () {
+    return view('perfil/sobre');
+});
+Route::get('/albuns', function () {
+    return view('perfil/albuns');
+});
+Route::get('/perfil-amigos', function () {
+    return view('perfil/amigos');
+});
+
+Route::group(["prefix"=>"/feed"], function() use ($router){
+   // Route::post('/conhecidos','AuthController@login');
+    Route::post('/logout', 'AuthController@logout');
+
+});
+
